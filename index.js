c5046cfff26319f97bf203b6e15613de1f384420
Gabri3l <gabriele.cimato@gmail.com>
2020-02-13 23:04:35 -0800

    refactor: add logs to track progress

--- index.js
+++ index.js
@@ -92,8 +92,12 @@ async function main() {
     tags
   } = await handleBlogPostUserInput();
   files.createPostTemplate(title, formatTitle, description, date, tags);
+  await new Promise(resolve => setTimeout(resolve, 3000));
+  log.print("Checking out branch...", { color: "blue" });
   await github.checkoutNewBranch(formatTitle);
+  log.print("Committing changes...", { color: "blue" });
   await github.commitChanges();
+  log.print("Pushing branch...", { color: "blue" });
   await github.pushChanges(formatTitle);
 }
 
