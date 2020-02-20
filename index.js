fb9840b5ffe3c6c6c580776c58d04e32c636449e
Gabri3l <gabriele.cimato@gmail.com>
2020-02-19 23:44:35 -0800

    style: add prettier + apply to all files

--- index.js
+++ index.js
@@ -1,89 +1,84 @@
 #!/usr/bin/env node
 
-const log = require("./lib/log");
-const { Spinner } = require("clui");
-const clear = require("clear");
-const path = require("path");
-const files = require("./lib/files");
-const inquirer = require("./lib/inquirer");
-const github = require("./lib/github");
+const log = require("./lib/log")
+const { Spinner } = require("clui")
+const clear = require("clear")
+const path = require("path")
+const files = require("./lib/files")
+const inquirer = require("./lib/inquirer")
+const github = require("./lib/github")
 
-const BLOG_POSTS_PATH = path.join("content", "blog");
+const BLOG_POSTS_PATH = path.join("content", "blog")
 
 async function handleBlogPostUserInput() {
-  const {
-    title,
-    description,
-    date,
-    tags
-  } = await inquirer.askBlogPostDetails();
+  const { title, description, date, tags } = await inquirer.askBlogPostDetails()
   const urlFormatTitle = title
     .toLowerCase()
     .split(" ")
-    .join("-");
+    .join("-")
 
-  files.createDirectory(path.join(BLOG_POSTS_PATH, urlFormatTitle));
+  files.createDirectory(path.join(BLOG_POSTS_PATH, urlFormatTitle))
 
   return Promise.resolve({
     title,
     urlFormatTitle,
     description,
     date,
-    tags
-  });
+    tags,
+  })
 }
 
 async function main() {
-  clear();
+  clear()
 
-  log.print("Create Blog Post", { color: "yellow", isTitle: true });
+  log.print("Create Blog Post", { color: "yellow", isTitle: true })
   log.print(
     `A simple CLI tool to create and schedule a blog post based on Gatsby blog starter.
       Make sure to run this from the root of your project.`,
     { color: "blue" }
-  );
+  )
 
   if (!files.isDirectoryGitRepo()) {
     log.errorAndExit(
       "This is not your root directory or it's not a valid git repo."
-    );
+    )
   }
 
   try {
-    await github.authenticateUser();
+    await github.authenticateUser()
 
     const {
       title,
       urlFormatTitle,
       description,
       date,
-      tags
-    } = await handleBlogPostUserInput();
+      tags,
+    } = await handleBlogPostUserInput()
     const newBlogPostFilePath = path.join(
       BLOG_POSTS_PATH,
       urlFormatTitle,
       `index.md`
-    );
+    )
     files.createPostTemplate(
       title,
       newBlogPostFilePath,
       description,
       date,
       tags
-    );
+    )
     const status = new Spinner(
       `Committing your changes to branch ${urlFormatTitle}...`
-    );
-    status.start();
-    await github.checkoutNewBranch(urlFormatTitle);
-    await github.add(newBlogPostFilePath);
-    await github.commit();
-    await github.push(urlFormatTitle);
-    status.stop();
-    await github.submiatPr(urlFormatTitle, date.split(" ")[0]);
+    )
+    status.start()
+    await github.checkoutNewBranch(urlFormatTitle)
+    await github.add(newBlogPostFilePath)
+    await github.commit()
+    await github.push(urlFormatTitle)
+    status.stop()
+    await github.submitPr(urlFormatTitle, date.split(" ")[0])
   } catch (error) {
-    log.error(error.message);
+    log.error(error.message)
   }
 }
 
-main();
+main()
